f94d4a04f1270a68254fbcbb94b785e388c42104
donowhy <donowhy11@gmail.com>
2016-02-18 17:55:19 -0800

    cordova integration

--- textingApp.js
+++ textingApp.js
@@ -1,6 +1,28 @@
 Contacts = new Mongo.Collection("contacts");
 Messages = new Mongo.Collection("messages");
 
+if (Meteor.isCordova) {
+  var app = {
+    sendSms: function() {
+      var number = document.getElementById('numberTxt').value;
+      var message = document.getElementById('messageTxt').value;
+      alert(number);
+      alert(message);
+
+      //CONFIGURATION
+      var options = {
+        replaceLineBreaks: false, // true to replace \n by a new line, false by default
+        android: {
+          //intent: 'INTENT'  // send SMS with the native android SMS messaging
+          intent: '' // send SMS without open any other app
+        }
+      };
+
+      var success = function () { alert('Message sent successfully'); };
+      var error = function (e) { alert('Message Failed:' + e); };
+      sms.send(number, message, options, success, error);
+    }
+  };}
 
 if (Meteor.isClient) {
   // counter starts at 0
